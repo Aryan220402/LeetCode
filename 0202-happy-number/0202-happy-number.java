8d35fb0633c656c4a8273cc3d547f0d3084588cd
Aryan Singh <79953816+Aryan220402@users.noreply.github.com>
2023-03-21 10:45:33 +0530

    Time: 1 ms (89.66%), Space: 39.7 MB (45.24%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,16 +1,21 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> ans = new HashSet<>();
-        while (n != 1 && !ans.contains(n)) {
-            ans.add(n);
-            int sum = 0;
-            while (n != 0) {
-                int digit = n % 10;
-                sum += digit * digit;
-                n /= 10;
+        List<Integer>list= new ArrayList<Integer>();
+        while(n!=1){
+            int tmp = n;
+            n=0;
+            while(tmp>0){
+                n=n+(int) Math.pow((tmp%10),2);
+                
+                tmp = tmp/10;
             }
-            n = sum;
+            if(list.contains(n)){
+                return false;
+            }
+            else{
+                list.add(n);    
+            }
         }
-        return n == 1;
+         return true; 
     }
 }
