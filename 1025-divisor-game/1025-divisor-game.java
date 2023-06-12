f922814549b9ba54079996ee36f5360a06d9de0c
Aryan Singh <79953816+Aryan220402@users.noreply.github.com>
2023-06-12 11:11:04 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (69.61%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.java
+++ 1025-divisor-game/1025-divisor-game.java
@@ -1,6 +1,17 @@
 class Solution {
     public boolean divisorGame(int n) {
+        //1st solution
         return n%2==0;
+        //second solution
+        // if(n<=1){
+        //     return false;
+        // }
+        // for(int i=0;i<n;x++){
+        //     if(n%x==0){
+        //         n=n-1
+        //     }
+        // }
+        // return true;
         
     }
 }
